70ce8262a9126f75d0e44bd96a282f494aa5d54c
Cristina Bulnes <cristinabulnesmaya@gmail.com>
2020-11-30 17:19:50 +0100

    calendar-funcionality

---
+++ calendar-funcionality.js
@@ -0,0 +1,17 @@
+/* function addNewEvent (title, day) {
+
+} */
+const previusButton = document.querySelector(".previus-button");
+const nextButton = document.querySelector(".next-button");
+
+const monthArray = ["January", "February", "March", "April", "May", "Jun", "July", "August", "September", "October", "November", "December"]
+
+var actualMonth = document.querySelector(".month").innerHTML;
+var actualYear = document.querySelector(".year").innerHTML;
+
+previusButton.addEventListener("click", displayMonths);
+nextButton.addEventListener("click", displayMonths);
+
+function displayMonths () {
+
+}
