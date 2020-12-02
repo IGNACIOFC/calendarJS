38a5fdb3e5df2c0436337e1d9e9e26c6b17263a4
Cristina Bulnes <cristinabulnesmaya@gmail.com>
2020-12-02 10:01:18 +0100

    funcion to create new objects

--- storage-nformation.js
+++ storage-nformation.js
@@ -1,25 +1,37 @@
-let newEvent = {
+
+function newEvent (title, startDate, endDate, remindTime, description, type) {
+    this.title = "";
+    this.startDate = new Date();
+    this.endDate = new Date();
+    this.remindTime = "";
+    this.description = "";
+    this.type = "";
+}
+
+/* let newEvent = {
     title: "",
     startDate: new Date(),
     endDate: new Date(),
     remindTime: "",
     description: "",
     type: ""
-}
+} */
 
 var myEvents = [];
 
 document.getElementById("createNewEventModalButton").addEventListener("click", storeEvent, true);
 
 function storeEvent () {
-    newEvent.title = document.getElementById("eventTitle").value;
-    newEvent.startDate = document.getElementById("date").value;
-    newEvent.endDate = document.getElementById("endDateCalendar").value;
-    newEvent.remindTime = document.getElementById("time").value;
-    newEvent.description = document.getElementById("description").value;
-    newEvent.type = document.getElementById("eventType").value;
+    var myNewEvent = new newEvent();
 
-    myEvents.push(newEvent);
+    myNewEvent.title = document.getElementById("eventTitle").value;
+    myNewEvent.startDate = document.getElementById("date").value;
+    myNewEvent.endDate = document.getElementById("endDateCalendar").value;
+    myNewEvent.remindTime = document.getElementById("time").value;
+    myNewEvent.description = document.getElementById("description").value;
+    myNewEvent.type = document.getElementById("eventType").value;
+
+    myEvents.push(myNewEvent);
 
     
     localStorage.setItem("myEvents", JSON.stringify(myEvents));
@@ -34,5 +46,17 @@ function storeEvent () {
     alert(newEvent.startDate);
     alert(newEvent.endDate);
     alert(newEvent.remindTime); */
+}
+
+function createEventContainer () {
+    const element = document.createElement("p");
+    let text = document.createTextNode("evento1")
+    element.appendChild(text);
+    
+}
+
+function showEventsInCalendar () {
+    JSON.parse(myeventinformation);
+    alert(myeventinformation[0].title);
 }
 
