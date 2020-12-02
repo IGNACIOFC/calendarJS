bc1c28f3c167ff5d3a3771fa95e05dd70a82eb60
Cristina Bulnes <cristinabulnesmaya@gmail.com>
2020-12-02 18:51:58 +0100

    eventinformation

--- show-event.js
+++ show-event.js
@@ -21,12 +21,22 @@ function showEventsInCalendar (month, year) {
                     let text = document.createTextNode(allMyEvents[index].title);
                     p.appendChild(text);
                     p.classList.add("calendar-event");
+                    p.setAttribute("id", index);
                     element.appendChild(p);
                     console.log("Eventos en un dia" + element.childNodes.length);
                 }
             });
         }
     }
+    myevents = document.querySelectorAll(".calendar-event");
+    myevents.forEach(element => {   
+    element.addEventListener("click", function () {
+        index = element.getAtribute("id");
+        document.getElementById("myEventodal").style.display = "block";
+        document.getElementById("eventtitle").innerHTML = allMyEvents[index].title
+
+    })
+});
 }
 
 
